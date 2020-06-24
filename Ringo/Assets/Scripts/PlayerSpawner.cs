125dc42fa09831c297463c0269a2c2362d2b6996
Jakub Hampl <jakub.hampl@email.cz>
2020-06-24 22:12:33 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Ringo/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [Header("Player 1")]
+    public GameObject playerOne;
+    public float playerOneX;
+    public float playerOneY;
+
+    [Header("Player 2")]
+    public GameObject playerTwo;
+    public float playerTwoX;
+    public float playerTwoY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(playerOne, new Vector3(playerOneX, playerOneY), Quaternion.identity);
+        Instantiate(playerTwo, new Vector3(playerTwoX, playerTwoY), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Ringo/Assets/Scripts/PlayerState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{ 
+    Idle,
+    HangingRight,
+    HangingLeft,
+    HangingRightUp,
+    HangingRightDown,
+    HangingLeftUp,
+    HangingLeftDown
+}
